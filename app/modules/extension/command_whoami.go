241eb348dc6dd6a87953f1d3bbe2ea9005e10544
Jeongbin Park <pjb7687@gmail.com>
2022-07-28 23:21:29 +0900

    Update command_whoami.go

--- app/modules/extension/command_whoami.go
+++ app/modules/extension/command_whoami.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 
 	gonduitRequests "github.com/etcinit/gonduit/requests"
-	"github.com/etcinit/phabulous/app/gonduit/extensions"
-	"github.com/etcinit/phabulous/app/gonduit/extensions/requests"
-	"github.com/etcinit/phabulous/app/interfaces"
-	"github.com/etcinit/phabulous/app/messages"
+	"github.com/pjb7687/phabulous/app/gonduit/extensions"
+	"github.com/pjb7687/phabulous/app/gonduit/extensions/requests"
+	"github.com/pjb7687/phabulous/app/interfaces"
+	"github.com/pjb7687/phabulous/app/messages"
 )
 
 // WhoamiCommand allows one to send test messages to the feed channel.
