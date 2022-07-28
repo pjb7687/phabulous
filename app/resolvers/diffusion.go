f76f7630c3fdec484372bad1a65a670c9a6b811b
Jeongbin Park <pjb7687@gmail.com>
2022-07-28 23:18:41 +0900

    Update diffusion.go

--- app/resolvers/diffusion.go
+++ app/resolvers/diffusion.go
@@ -2,7 +2,7 @@ package resolvers
 
 import (
 	"github.com/etcinit/gonduit/requests"
-	"github.com/etcinit/pjb7687/app/factories"
+	"github.com/pjb7687/phabulous/app/factories"
 	"github.com/jacobstr/confer"
 )
 
