a251f0444b63f3e1e4df775892b6c470304d55af
Jeongbin Park <pjb7687@gmail.com>
2022-07-28 23:12:20 +0900

    Update command_modules.go

--- app/modules/core/command_modules.go
+++ app/modules/core/command_modules.go
@@ -3,8 +3,8 @@ package core
 import (
 	"fmt"
 
-	"github.com/etcinit/phabulous/app/interfaces"
-	"github.com/etcinit/phabulous/app/messages"
+	"github.com/pjb7687/phabulous/app/interfaces"
+	"github.com/pjb7687/phabulous/app/messages"
 )
 
 // ModulesCommand allows one to send test messages to the feed channel.
